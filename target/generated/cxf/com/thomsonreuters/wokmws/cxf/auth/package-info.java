ddc3497f1ec3b5542ddd3af185d1a90c548f1e4a
kamciak <kamciak2@wp.pl>
2015-01-29 18:02:52 +0100

    Usuniecie katalogu target
    Moze wkoncu zacznie to dzialac

--- target/generated/cxf/com/thomsonreuters/wokmws/cxf/auth/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://auth.cxf.wokmws.thomsonreuters.com")
-package com.thomsonreuters.wokmws.cxf.auth;
--- target/generated/cxf/com/thomsonreuters/wokmws/v3/woksearch/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://woksearch.v3.wokmws.thomsonreuters.com")
-package com.thomsonreuters.wokmws.v3.woksearch;
