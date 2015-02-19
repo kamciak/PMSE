9f5ee91ca6e9deb25666120bfd3b0fa200651101
kamciak <kamciak2@wp.pl>
2015-02-19 20:30:12 +0100

    Update

--- src/main/java/com/publicationmetasearchengine/services/datacollectorservice/bwn/BWNDataCollector.java
+++ src/main/java/com/publicationmetasearchengine/services/datacollectorservice/bwn/BWNDataCollector.java
@@ -213,7 +213,7 @@ public class BWNDataCollector implements ServiceJobProvider, Serializable {
                         record.getTitle(),
                         record.getSummary(),
                         record.getDOI(),
-                        null, ////////////////////////////////////////////JOURNAL_REF ?????????????
+                        record.getJournalRef(), ////////////////////////////////////////////JOURNAL_REF ?????????????
                         sourceTitleId,
                         record.getSourceInfo().getVolumeId(),
                         record.getSourceInfo().getIssueId(),
